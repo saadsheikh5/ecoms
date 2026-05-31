ba890901a8ce6effc0e0eebf505c4421185e2940
saadsheikh5 <saadalisheikh2002@gmail.com>
2026-05-31 17:27:48 +0500

    Refresh cached catalog from admin updates

--- src/constants/apiCache.js
+++ src/constants/apiCache.js
@@ -1,24 +1,5 @@
 export const cachedProducts = [
   {
-    "_id": "6a1c228688a94faac51c4687",
-    "title": "Lace bond glue",
-    "description": "",
-    "category": "Lace Glues",
-    "price": 7.5,
-    "stock": 10,
-    "image": "https://res.cloudinary.com/dxti8btbn/image/upload/v1780228742/jts-beauty/products/twnrlmkp4rhlmnhmmtfi.jpg",
-    "images": [
-      "https://res.cloudinary.com/dxti8btbn/image/upload/v1780228742/jts-beauty/products/twnrlmkp4rhlmnhmmtfi.jpg",
-      "https://res.cloudinary.com/dxti8btbn/image/upload/v1780229018/jts-beauty/products/kr8p4qcvwvictdomugyk.jpg",
-      "https://res.cloudinary.com/dxti8btbn/image/upload/v1780229084/jts-beauty/products/e3jytgv2rafi7m4qqn4c.jpg"
-    ],
-    "isFeatured": false,
-    "variants": [],
-    "createdAt": "2026-05-31T11:59:02.681Z",
-    "updatedAt": "2026-05-31T12:04:44.565Z",
-    "__v": 0
-  },
-  {
     "_id": "6a1b20983444dd59a7cb3ff6",
     "title": "Bob Wig - Dark",
     "description": "Bob Wig - Dark\r\n• 13x4  \r\n• Transparent Lace",
@@ -1188,17 +1169,14 @@ export const cachedProducts = [
     "category": "Lace Tints",
     "price": 10,
     "stock": 10,
-    "image": "https://res.cloudinary.com/dxti8btbn/image/upload/v1780171220/jts_beauty/products/ltle9c0x87omfkb3tesy.jpg",
+    "image": "https://res.cloudinary.com/dxti8btbn/image/upload/v1780171220/jts_beauty/products/giuv0cnfcaeoeh4liggk.jpg",
     "images": [
-      "https://res.cloudinary.com/dxti8btbn/image/upload/v1780171220/jts_beauty/products/giuv0cnfcaeoeh4liggk.jpg",
-      "https://res.cloudinary.com/dxti8btbn/image/upload/v1780171222/jts_beauty/products/fao2a1sfojaxp5yvrqcq.jpg",
-      "https://res.cloudinary.com/dxti8btbn/image/upload/v1780171222/jts_beauty/products/rnda1pjsa0o0xtvznwzg.jpg",
-      "https://res.cloudinary.com/dxti8btbn/image/upload/v1780170754/jts-beauty/products/zsneygczovmtsiuywjg7.jpg"
+      "https://res.cloudinary.com/dxti8btbn/image/upload/v1780171220/jts_beauty/products/giuv0cnfcaeoeh4liggk.jpg"
     ],
     "isFeatured": false,
     "variants": [],
     "createdAt": "2026-05-26T20:51:18.645Z",
-    "updatedAt": "2026-05-30T20:00:23.344Z",
+    "updatedAt": "2026-05-31T12:25:47.630Z",
     "__v": 1
   },
   {
@@ -1880,4 +1858,4 @@ export const cachedProducts = [
     "updatedAt": "2026-05-30T20:10:33.919Z"
   }
 ];
-export const cachedProductsUpdatedAt = "2026-05-31T12:20:11.930Z";
+export const cachedProductsUpdatedAt = "2026-05-31T12:26:19.840Z";
